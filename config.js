e66c2c507ba5d0ae52c6f2aef039abc54e0ccd6b
affilinno <affilinno@gmail.com>
2026-01-10 16:40:07 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
   // GAS Web App URL（デプロイ後に設定）
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbw-G-57o1GHB1ZtumauNWxLeV_ngzEot-Ahx6wq7UbIvKQjIXiIY0vVy_A0LwDZVc1HsA/exec',
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbxvpntVy6pZTv8-1XP3g77Zmjw8X2C169bAi6GKgIfHjnWZtAAyrY6VwlnsDKZ_k3A-/exec',
   
   // ダッシュボード認証用APIキー
   API_KEY: 'AFFILINNO',
